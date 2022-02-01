cefd529a30f25cb98314a610735caadcfb95ba52
Tom Virgo <tomcvirgo@gmail.com>
2022-02-01 16:36:45 +0000

    create custom endpoint to add new post with user email

--- newsletter/posts/views.py
+++ newsletter/posts/views.py
@@ -2,13 +2,27 @@ from rest_framework import viewsets
 from .models import Post, CommentLike, Comment, PostLike, Suggestion, SuggestionComment, SuggestionVote
 from .serializers import PostSerializer, PostLikeSerializer, CommentLikeSerializer, CommentSerializer, SuggestionSerializer, SuggestionCommentSerializer, SuggestionVoteSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.contrib.auth import get_user_model
 
 class PostViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    # permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+
+    @action(detail=False, methods=['post'])
+    def add_post_by_email(self, request):
+        user_model = get_user_model()
+        user = user_model.objects.get(email=request.data['author']) # use get when getting single object, use filter when getting multiple
+        new_data = request.data
+        new_data['author'] = user.id
+        serializer = PostSerializer(data=new_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
