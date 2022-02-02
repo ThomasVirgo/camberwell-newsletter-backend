0d98db33fa6e40bdeeacc93280be8fbe2b17bf05
Tom Virgo <tomcvirgo@gmail.com>
2022-02-02 16:20:01 +0000

    add endpoint for creating comment by user email

--- newsletter/posts/views.py
+++ newsletter/posts/views.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 
 class PostViewSet(viewsets.ModelViewSet):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     serializer_class = PostSerializer
     queryset = Post.objects.all()
 
@@ -33,6 +33,18 @@ class CommentViewSet(viewsets.ModelViewSet):
         comments = Comment.objects.filter(post=pk)
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+    
+    @action(detail=False, methods=['post'])
+    def add_comment_by_email(self, request):
+        user_model = get_user_model()
+        user = user_model.objects.get(email=request.data['author'])
+        new_data = request.data
+        new_data['author'] = user.id
+        serializer = CommentSerializer(data=new_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CommentLikeViewSet(viewsets.ModelViewSet):
     serializer_class = CommentLikeSerializer
